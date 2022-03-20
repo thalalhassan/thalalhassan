1dc267932c132cc909ab8b18952d70b4db0b2351
thalalhassan <thalal424@gmail.com>
2022-03-20 16:00:40 +0530

    updates

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,6 +1,6 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://thalalhassan.github.io/',
+  homepage: 'https://thalalhassan.github.io/thalalhassan',
   title: 'THR.',
 }
 
